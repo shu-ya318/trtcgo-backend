2c23baa2507d94f3832176cbbc7fa955df1adb56
shu-ya318 <funghi0983524367@gmail.com>
2026-05-15 12:00:58 +0800

    initial commit

---
+++ src/main/java/com/mli/trtcgo/backend/TrtcgoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mli.trtcgo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrtcgoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrtcgoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mli/trtcgo/backend/TrtcgoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mli.trtcgo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrtcgoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
